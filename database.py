df1b4faacd9675ef472c8a8ac6540d41fc0a16d4
Sanskar Biyani <sanskarbiyani902@gmail.com>
2022-02-09 18:29:15 +0530

    DashBoard, Book Detail, Chapter Web-Page added.

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ import mysql.connector
 from mysql.connector import errorcode
 import os
 
-DB_NAME = "library_management"
+DB_NAME = "WTLLab"
 
 
 class Database:
@@ -16,16 +16,20 @@ class Database:
         self.cursor = self.conn.cursor()
 
     def get_user(self, userId):
-        string = f"SELECT ID FROM customer WHERE ID={userId}"
+        string = f"SELECT ID, is_member, is_librarian FROM customer WHERE ID={userId}"
         self.cursor.execute(string)
         res = self.cursor.fetchall()
-        print(res)
+        return res
+
+    def get_user_by_username(self, username):
+        string = f"""SELECT ID, Is_Member, Is_Librarian FROM customer WHERE customerName='{username}'"""
+        self.cursor.execute(string)
+        res = self.cursor.fetchall()
         return res
 
     def add_user(self, username, password):
         string = f"""INSERT INTO customer(customerName, Password) 
                 VALUES ('{username}', '{password}')"""
-        print(string)
         try:
             self.cursor.execute(string)
         except mysql.connector.Error as err:
@@ -39,7 +43,40 @@ class Database:
             res = self.cursor.fetchall()[0]
             cust_id = res[0]
             password = res[1]
-            print(password)
             return password, cust_id
         except mysql.connector.Error as err:
             print(err)
+
+    def get_all_books(self):
+        string = f"""SELECT * from book"""
+        self.cursor.execute(string)
+        res = self.cursor.fetchall()
+        return res
+
+    def get_book_by_isbn(self, isbn):
+        string = f"""SELECT * FROM book WHERE isbn='{isbn}'"""
+        self.cursor.execute(string)
+        res = self.cursor.fetchall()
+        return res
+
+    def get_reviews(self, isbn):
+        string = f"""SELECT r.review_text, c.customerName
+                    FROM review as r
+                    INNER JOIN customer AS c ON r.user_id = c.ID
+                    INNER JOIN book AS b ON r.book_id = b.isbn
+                    WHERE r.book_id={isbn}"""
+        self.cursor.execute(string)
+        res = self.cursor.fetchmany(size=10)
+        return res
+
+    def search_book(self, book_name):
+        string = f"""SELECT * FROM book WHERE Name LIKE '%{book_name}%'"""
+        self.cursor.execute(string)
+        res = self.cursor.fetchall()
+        return res
+
+    def get_chapter(self, isbn, chapter):
+        string = f"""SELECT * FROM chapter WHERE book_id={isbn} AND chapter_id={chapter}"""
+        self.cursor.execute(string)
+        res = self.cursor.fetchall()
+        return res
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask, redirect, render_template, request, url_for
+from cgitb import reset
+from functools import wraps
+from flask import Flask, redirect, render_template, request, url_for, abort
 from flask_login import LoginManager, current_user, login_required, login_user, UserMixin, logout_user
 from dotenv import load_dotenv
 from database import Database
@@ -16,15 +18,33 @@ mydb = Database()
 
 
 class User(UserMixin):
-    pass
+    def __init__(self, user_id, is_member, is_librarian) -> None:
+        super().__init__()
+        self.id = user_id
+        self.is_member = is_member
+        self.is_librarian = is_librarian
+
+
+def librarian_only(func):
+    @wraps(func)
+    def decorated_function(*args, **kwargs):
+        if not current_user.is_authenticated or current_user.is_librarian != 1:
+            return abort(403)
+        else:
+            return func(*args, **kwargs)
+    return decorated_function
 
 
 @login_manager.user_loader
 def load_user(user_id):
-    print("Creating session.")
-    user = User()
-    user.id = user_id
-    return user
+    print("Inside Load User.")
+    user_details = mydb.get_user(user_id)
+    if len(user_details) == 0:
+        return None
+    else:
+        curr_user_id, is_member, is_librarian = user_details[0]
+        user = User(curr_user_id, is_member, is_librarian)
+        return user
 
 
 @app.route("/")
@@ -34,24 +54,31 @@ def home_page():
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
-    if request.method == 'GET':
-        return render_template('login.html')
-    else:
-        username = request.form['username']
-        password = request.form['password']
-        hashed, cust_id = mydb.get_password(username)
-        if bcrypt.checkpw(str(password).encode('utf-8'), bytes(hashed)):
-            print("Password Matched.")
-            user = User()
-            user.id = cust_id
-            login_user(user)
-            return redirect(url_for('dashboard'))
+    if not current_user.is_authenticated:
+        if request.method == 'GET':
+            return render_template('login.html')
         else:
-            print("Password not matching.")
-        return 'None'
+            username = request.form['username']
+            password = request.form['password']
+            hashed, cust_id = mydb.get_password(username)
+            if bcrypt.checkpw(str(password).encode('utf-8'), bytes(hashed)):
+                print('Password Matched.')
+                user_details = mydb.get_user_by_username(username)
+                if(len(user_details) == 0):
+                    return redirect(url_for('login'))
+                else:
+                    user_id, is_member, is_librarian = user_details[0]
+                user = User(user_id, is_member, is_librarian)
+                login_user(user)
+                return redirect(url_for('dashboard'))
+            else:
+                print("Password not matching.")
+                return 'None'
+    else:
+        return redirect("dashboard")
 
 
-@app.route("/logout", methods=['POST'])
+@app.route("/logout", methods=['GET'])
 @login_required
 def logout():
     logout_user()
@@ -65,13 +92,58 @@ def register():
     hashed_password = bcrypt.hashpw(
         str(unhash).encode('utf-8'), bcrypt.gensalt())
     mydb.add_user(username, hashed_password.decode())
-    return "None"
+    user_details = mydb.get_user_by_username(username)
+    if(len(user_details) == 0):
+        return redirect(url_for('login'))
+    else:
+        user_id, is_member, is_librarian = user_details[0]
+    user = User(user_id, is_member, is_librarian)
+    login_user(user)
+    return redirect(url_for('dashboard'))
 
 
 @app.route("/dashboard")
 @login_required
 def dashboard():
-    return render_template('dashboard.html')
+    all_books = mydb.get_all_books()
+    return render_template('dashboard.html', books=all_books)
+
+
+@app.route("/book/<int:isbn>", methods=['GET'])
+def get_book(isbn):
+    book_detail = mydb.get_book_by_isbn(isbn)
+    if(len(book_detail) == 0):
+        abort(404)
+    else:
+        result = {}
+        result["book"] = book_detail[0]
+        result["reviews"] = mydb.get_reviews(isbn)
+        print(result['reviews'])
+        return render_template('book.html', result=result)
+
+
+@app.route("/search", methods=['GET'])
+@login_required
+def search_book():
+    book_name = request.form['bookname']
+    if(book_name and len(book_name) > 0):
+        res = mydb.search_book(book_name)
+        return "Finding Book"
+
+
+@app.route("/book/<int:isbn>/chapter/<int:chapter_no>", methods=['GET'])
+@login_required
+def get_chapter(isbn, chapter_no):
+    chapter_text = mydb.get_chapter(isbn, chapter_no)
+    if(len(chapter_text) > 0):
+        return render_template('chapter.html', chapter=chapter_text[0])
+
+
+@app.route("/admin")
+@login_required
+@librarian_only
+def admin():
+    return "Admin Page."
 
 
 if __name__ == "__main__":
